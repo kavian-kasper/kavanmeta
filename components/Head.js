8e2eae57d1cfc5613710b7bb8a5cd3b2e4915283
kavian-kasper <60392158+kavian-kasper@users.noreply.github.com>
2021-11-18 14:36:54 +0100

    academicMetatags

--- components/Head.js
+++ components/Head.js
@@ -11,6 +11,20 @@ export const HeadInfo = (props) => {
         <meta name="description" content="KavanMeta is creating graphics for the coming metaverse."/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.gif" />
+
+        <meta property="og:title" content="Metatoons - NFTs by KavanMeta" />
+                    <meta property="og:url" content="https://kavanmeta.io" />
+                    <meta property="og:image" content="https://kavanmeta.vercel.app/social.jpg" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:description" content="Bespoke 3D Avatars primed for a future metaverse."/>
+
+                    <meta name="twitter:card" content="summary_large_image"/>
+                    <meta name="twitter:site" content="@kavanmeta"/>
+                    <meta name="twitter:url" content="https://kavanmeta.vercel.app/"/>
+                    <meta name="twitter:title" content="Metatoons - NFTs by KavanMeta"/>
+                    <meta name="twitter:description" content="Bespoke 3D Avatars primed for a future metaverse."/>
+                    <meta name="twitter:image" content="https://kavanmeta.vercel.app/social.jpg/"/>
+
       </Head>
       <Script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></Script>
       </>
